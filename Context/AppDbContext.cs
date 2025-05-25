cef4ede2350507a57f98343a44f004ef3f4e0a4f
henriquesantos14 <henriquepsouza.santos@gmail.com>
2025-05-25 00:28:21 -0300

    Add files via upload

---
+++ Context/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PaulaPachecoHairStyle.Models;
+
+namespace PaulaPachecoHairStyle.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        { 
+
+        }
+
+        public DbSet<Photo> Photo { get; set; }
+        public DbSet<Procedure> Procedures { get; set; } 
+        public DbSet<Appointment> Appointments { get; set; }
+    }
+}
